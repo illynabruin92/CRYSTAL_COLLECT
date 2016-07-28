286122ac6dfde145fcd61d0947954afeb64602e3
illynabruin92 <alvaivette@g.ucla.edu>
2016-07-28 12:01:01 -0700

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+/*  1. At start of game, generate random number and display.
+2. Assign a random value to each diamond. Each time you click on diamond, you add to your total score.
+3. If total number of clicks matches random number, add one tally to wins, and display "You won!"
+4. If total number of clicks is higher than random number, add one tally to losses, and display "You lost!". */
+
+//$(document).ready(function() {
+  //var numberToGuess = Math.floor(Math.random() * 50 + 1);
+
+  //var winCounter = 0
+
+  //var lossCounter = 0
+
+  //var scoreCounter = 0
+
+//  $("#random-number").text(numberToGuess);});
+
+// GLOBAL VARIABLES (Accessible by all functions)
+
+//Random Number Generator Variable
+//var randomNumber = [Math.floor(Math.random() * 100 + 1)];
+
+
+// FUNCTIONS
+
+//function startGame() {
+  // Reset the score back to 0
+//  scoreCounter = 0
+
+//  $('#random-number').html(randomNumber);
+
+
+
+}
+
+//startGame();
